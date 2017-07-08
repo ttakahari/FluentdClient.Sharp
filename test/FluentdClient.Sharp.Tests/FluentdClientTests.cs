da36ea4bd7a7555000086827cf09bd090b452507
t.takahari <taiga1018@gmail.com>
2017-07-09 01:27:29 +0900

    add tests.

---
+++ test/FluentdClient.Sharp.Tests/FluentdClientTests.cs
@@ -0,0 +1,110 @@
+﻿using FluentdClient.Sharp.MsgPackCli;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FluentdClient.Sharp.Tests
+{
+    public class FluentdClientTests
+{
+        private const string Host = "localhost";
+        private const int Port    = 24224;
+        private readonly IMessagePackSerializer _serializer = new MsgPackCliSerializer();
+
+        [Fact]
+        public void Constructor_Tests()
+        {
+            Assert.Throws<ArgumentNullException>(() => new FluentdClient("", Port, _serializer));
+            Assert.Throws<ArgumentNullException>(() => new FluentdClient(Host, Port, null));
+
+            using (var client = new FluentdClient(Host, Port, _serializer))
+            {
+                Assert.NotNull(client);
+            }
+
+            Assert.Throws<ArgumentNullException>(() => new FluentdClient(null));
+
+            using (var client = new FluentdClient(new FluentdSetting(Host, Port, _serializer)))
+            {
+                Assert.NotNull(client);
+            }
+        }
+
+        [Fact]
+        public async Task ConnectAsync_Tests()
+        {
+            using (var client = new FluentdClient(new FluentdSetting("172.0.0.1", Port, _serializer) { Timeout = 1000, ExceptionHandler = ex => { Assert.NotNull(ex); } }))
+            {
+                await client.ConnectAsync();
+            }
+
+            using (var client = new FluentdClient(Host, Port, _serializer))
+            {
+                await client.ConnectAsync();
+            }
+        }
+
+        [Fact]
+        public async Task SendAsync_Test()
+        {
+            using (var client = new FluentdClient(Host, Port, _serializer))
+            {
+                // Dictionary
+                {
+                    var message = new Dictionary<string, object>
+                    {
+                        { "Id"          , 100 },
+                        { "Name"        , "AAA" },
+                        { "Timestamp"   , DateTimeOffset.Now },
+                        { "IsAnonymous" , false },
+                        { "IsDictionary", true }
+                    };
+
+                    await client.SendAsync("test.aaa", message);
+                }
+
+                // Anonymous
+                {
+                    var message = new
+                    {
+                        Id           = 200,
+                        Name         = "BBB",
+                        Timestamp    = DateTimeOffset.Now,
+                        IsAnonymous  = true,
+                        IsDictionary = false
+                    };
+
+                    await client.SendAsync("test.bbb", message);
+                }
+
+                // Class
+                {
+                    var message = new Payload
+                    {
+                        Id           = 300,
+                        Name         = "CCC",
+                        Timestamp    = DateTimeOffset.Now,
+                        IsAnonymous  = false,
+                        IsDictionary = false
+                    };
+
+                    await client.SendAsync("test.ccc", message);
+                }
+            }
+        }
+
+        private class Payload
+        {
+            public int Id { get; set; }
+
+            public string Name { get; set; }
+
+            public DateTimeOffset Timestamp { get; set; }
+
+            public bool IsAnonymous { get; set; }
+
+            public bool IsDictionary { get; set; }
+        }
+    }
+}
---
+++ test/FluentdClient.Sharp.Tests/FluentdSettingTests.cs
@@ -0,0 +1,38 @@
+﻿using FluentdClient.Sharp.MsgPackCli;
+using System;
+using Xunit;
+
+namespace FluentdClient.Sharp.Tests
+{
+    public class FluentdSettingTests
+    {
+        [Fact]
+        public void Tests()
+        {
+            const string host = "localhost";
+            const int port    = 24224;
+            const int timeout = 1000;
+
+            var serializer      = new MsgPackCliSerializer();
+            var exceptonHandler = new Action<Exception>(ex => { });
+
+            Assert.Throws<ArgumentNullException>(() => new FluentdSetting("", port, serializer));
+            Assert.Throws<ArgumentNullException>(() => new FluentdSetting(host, 0, null));
+
+            var setting = new FluentdSetting(host, port, serializer);
+
+            Assert.NotNull(setting);
+            Assert.Equal(setting.Host, host);
+            Assert.Equal(setting.Port, port);
+            Assert.Equal(setting.Serializer.GetType(), serializer.GetType());
+            Assert.False(setting.Timeout.HasValue);
+            Assert.Null(setting.ExceptionHandler);
+
+            setting.Timeout          = timeout;
+            setting.ExceptionHandler = exceptonHandler;
+
+            Assert.Equal(setting.Timeout, timeout);
+            Assert.NotNull(setting.ExceptionHandler);
+        }
+    }
+}
